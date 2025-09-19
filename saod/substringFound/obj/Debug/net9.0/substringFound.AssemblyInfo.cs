62a3c90debdbc4bfbf7c8dd1af3dfc614e79e214
UdZiRoN <rudometow06@gmail.com>
2025-09-19 10:29:53 +0700

    hzz

--- saod/substringFound/obj/Debug/net9.0/substringFound.AssemblyInfo.cs
+++ saod/substringFound/obj/Debug/net9.0/substringFound.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("substringFound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd87e451839bc378ba1447f77faa31ff4b313b94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef174c2a0bf01697cca6331e60998e5fcb308a46")]
 [assembly: System.Reflection.AssemblyProductAttribute("substringFound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("substringFound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
